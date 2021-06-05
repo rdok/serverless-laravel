6e3270e5996169ade6f7281afd9bf78a2575e58e
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-05 19:28:51 +0100

    Render image on the backend as base64.

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="author" content="Rizart Dokollari">
-        <title>AWS SAM - Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Rizart Dokollari">
+    <title>AWS SAM - Laravel</title>
 
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
-    </head>
-    <body class="main-content">
-    <header>
-        <h1>Serverless Laravel</h1>
-    </header>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}"/>
+</head>
+<body class="main-content">
+<header>
+    <h1>Serverless Laravel</h1>
+</header>
 
-    <img src="{{ asset('img/infrastructure.webp') }}" alt="Infrastructure Design for AWS SAM Laravel">
-        <script src="{{ mix('/js/app.js') }}"></script>
-    </body>
+<img src="{{ asset('img/infrastructure.webp') }}" alt="Infrastructure Design for AWS SAM Laravel">
+
+<img
+    src="data:image/jpg;base64,{!!  base64_encode(Storage::get('showcase-storage-retrieval.jpg')) !!}"
+    alt="Showcase getting file from storage."/>
+
+<script src="{{ mix('/js/app.js') }}"></script>
+</body>
 </html>
