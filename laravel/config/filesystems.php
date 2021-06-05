9f35c111394b46450f4fd47474338336b2777859
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-05 20:32:19 +0100

    Add s3 token.
    https://github.com/laravel/laravel/pull/5138

--- laravel/config/filesystems.php
+++ laravel/config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -50,6 +50,7 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+            'token' => env('AWS_SESSION_TOKEN'),
         ],
 
     ],
