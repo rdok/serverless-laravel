1d29230cce06c4e956bfaaaefb4260b830363ed6
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-06 21:37:14 +0100

    Link images to source code.

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -13,13 +13,22 @@
     <h1>Serverless Laravel</h1>
 </header>
 
-<img style="border: 1px solid #555;" src="{{ asset('img/infrastructure.jpg') }}" alt="Infrastructure Design for AWS SAM Laravel">
+<a href="https://github.com/rdok/serverless-laravel/blob/bb42873bbeb80c63985b4926d853d66bb794e0eb/laravel/resources/views/welcome.blade.php#L16">
+    <img
+        style="border: 1px solid #555; width: 100%"
+        src="{{ asset('img/infrastructure.jpg') }}"
+        alt="Infrastructure Design for AWS SAM Laravel"
+    />
+</a>
 
 <hr>
 
-<img
-    src="data:image/jpg;base64,{!!  base64_encode(Storage::get('showcase-storage-retrieval.jpg')) !!}"
-    alt="Showcase getting file from storage."/>
+<a href="https://github.com/rdok/serverless-laravel/blob/bb42873bbeb80c63985b4926d853d66bb794e0eb/laravel/resources/views/welcome.blade.php#L21">
+    <img
+        style="width: 100%"
+        src="data:image/jpg;base64,{!!  base64_encode(Storage::get('showcase-storage-retrieval.jpg')) !!}"
+        alt="Showcase getting file from storage."/>
+</a>
 
 <script src="{{ mix('/js/app.js') }}"></script>
 </body>
