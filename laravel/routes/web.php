7fa116103fa4a61c925cbc44b8570fa266582778
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-14 09:01:31 +0100

    Propagate website views counter.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Stat;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $stat = Stat::where(['name' => 'website_views'])->find(1);
+    $counter = $stat ? $stat->counter : 0;
+    return view('welcome', ['websiteViewsCounter' => $counter]);
 });
